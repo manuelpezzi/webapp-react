104f3657bbb66d680e60dae1806cf21d06e4870f
manuelpezzi <pezzimanuel@gmail.com>
2025-03-18 12:02:44 +0100

    hompage update complete

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,42 @@
+import MovieCard from '../components/MovieCard'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 export default function HomePage() {
+    const [movies, setMoives] = useState([]);
+
+    const fetchMovies = () => {
+        console.log('fetching movies...');
+
+        axios.get('http://localhost:3000/movies')
+            .then((res) => {
+                setMoives(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    }
+    const renderMovies = () => {
+        return movies.map((movie) => {
+            return (
+                <div className="col" key={movie.id}>
+                    <MovieCard movie={movie} />
+                </div>
+            );
+        });
+    };
+    useEffect(fetchMovies, []);
+
     return (
         <>
-            <h1 className="text-primary">Bool Movies</h1>
-            <h2>Qui andranno tutti i film</h2>
-            <div className="row row-cols-3">{/* Qui andranno tutti i film */}</div>
+            <h1 className="text-primary">Boolmovies</h1>
+            <h2>Tutti i nostri film</h2>
+            <div className="row row-cols-3">
+
+                {renderMovies()}
+
+
+            </div>
         </>
     );
 }
