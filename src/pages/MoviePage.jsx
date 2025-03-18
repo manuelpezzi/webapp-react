fa93e6b59aae73f13f7cbfec62cabc7b64b31b33
manuelpezzi <pezzimanuel@gmail.com>
2025-03-18 12:27:06 +0100

    MoivePage update complete fine repo

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,7 +1,34 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+//uso del compoennte ReviewCard
+import ReviewCard from '../components/ReviewCard';
 export default function MoviePage() {
+
+    const { id } = useParams();
+    const [movie, setMovie] = useState({});
+    const fetchMovie = () => {
+        axios.get(`http://localhost:3000/movies/${id}`)
+            .then((res) => setMovie(res.data))
+            .catch((error) => console.log(error));
+    };
+    useEffect(fetchMovie, [id]);
+
+    const renderReviews = () => {
+        return movie.reviews?.map((review) => {
+            return <ReviewCard key={review.id} review={review} />;
+        });
+    };
     return (
         <>
-            <h1>tiolo singolo film</h1>
+            <h1>{movie?.title}</h1>
+            <img src={movie?.image} alt={movie?.title} />
+            {/* qui andrà la pagina di dettaglio del prodotto */}
+
+            <section>
+                <h4>Our community reviews</h4>
+                {renderReviews()}
+            </section>
         </>
     );
 }
